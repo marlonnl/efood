d88921aa6d6bc29fdf6f3d0e51eb710956480ee0
Marlonn Locatelli <marlonn@gmail.com>
2024-10-29 11:03:09 -0300

    ADD: images e mapping da lista de restaurantes.

--- src/components/RestaurantCard/index.tsx
+++ src/components/RestaurantCard/index.tsx
@@ -1,4 +1,3 @@
-import japonesaImg from '../../assets/images/japonesa.png'
 import {
   Card,
   CardContainer,
@@ -13,28 +12,39 @@ import star from '../../assets/images/estrela.png'
 import Tag from '../Tag'
 import { SaibaMais } from '../Tag/styles'
 
-const RestaurantCard = () => (
+type Props = {
+  name: string
+  description: string
+  style: string
+  image: string
+  rating: number
+  destaque: boolean
+}
+
+const RestaurantCard = ({
+  name,
+  style,
+  rating,
+  destaque,
+  image,
+  description
+}: Props) => (
   <Card>
-    <img src={japonesaImg} alt="nome do restaurante" />
+    <img src={image} alt="nome do restaurante" />
     <Tags>
-      <Tag>Destaque da semana</Tag>
-      <Tag>Japonesa</Tag>
+      {destaque && <Tag>Destaque da semana</Tag>}
+      <Tag>{style}</Tag>
     </Tags>
 
     <CardContainer>
       <CardHeader>
-        <Nome>Nome do restaurante</Nome>
+        <Nome>{name}</Nome>
         <Nota>
-          <p>4.9</p>
+          <p>{rating}</p>
           <img src={star} />
         </Nota>
       </CardHeader>
-      <Description>
-        Peça já o melhor da culinária japonesa no conforto da sua casa! Sushis
-        frescos, sashimis deliciosos e pratos quentes irresistíveis. Entrega
-        rápida, embalagens cuidadosas e qualidade garantida. Experimente o Japão
-        sem sair do lar com nosso delivery!
-      </Description>
+      <Description>{description}</Description>
       <SaibaMais>Saiba mais</SaibaMais>
     </CardContainer>
   </Card>

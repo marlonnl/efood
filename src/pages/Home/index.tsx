43dc871a90e0ff15a0fdf93661719d1655f671da
Marlonn Locatelli <marlonn@gmail.com>
2024-10-29 11:05:23 -0300

    ADD: page Home.

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import Hero from '../../containers/Hero'
+import RestaurantList from '../../containers/RestaurantList'
+
+const Home = () => (
+  <>
+    <Hero />
+    <RestaurantList />
+  </>
+)
+
+export default Home

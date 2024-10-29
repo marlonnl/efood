abd0be0406bd1e0f38ddb87752ccea711fd4f313
Marlonn Locatelli <marlonn@gmail.com>
2024-10-29 17:14:46 -0300

    ADD: componente Menu na página do Perfil.

--- src/pages/Perfil/index.tsx
+++ src/pages/Perfil/index.tsx
@@ -1,10 +1,12 @@
 import Banner from '../../components/Banner'
 import Header from '../../components/Header'
+import Menu from '../../containers/Menu'
 
 const Perfil = () => (
   <>
     <Header />
     <Banner />
+    <Menu />
   </>
 )
 

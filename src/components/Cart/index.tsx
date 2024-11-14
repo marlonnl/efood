2ae46088b68fb381a7fc60b4bf611adb9493d1fc
Marlonn Locatelli <marlonn@gmail.com>
2024-11-14 16:25:14 -0300

    feat(Checkout): esqueleto da página Checkout.

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -5,10 +5,14 @@ import { remove, close } from '../../store/reducers/cart'
 
 import { CartContainer, CartItem, Overlay, Sidebar, Subtotal } from './styles'
 import { priceFormat } from '../Item'
+import { useState } from 'react'
+import Checkout from '../../pages/Checkout'
 
 const Cart = () => {
   const { isOpen, items } = useSelector((state: RootReducer) => state.cart)
   const dispatch = useDispatch()
+
+  const [showCheckout, setShowCheckout] = useState(false)
 
   const closeCart = () => {
     dispatch(close())
@@ -20,36 +24,45 @@ const Cart = () => {
     }, 0)
   }
 
+  const goCheckout = () => {
+    setShowCheckout(true)
+    closeCart()
+  }
+
   return (
-    <CartContainer className={isOpen ? 'is-open' : ''}>
-      <Overlay onClick={() => closeCart()} />
-      <Sidebar>
-        {items.length < 1 ? (
-          <p>Não há ítens no seu carrinho.</p>
-        ) : (
-          items.map((item) => (
-            <>
-              <CartItem key={item.id}>
-                <img src={item.foto} alt="oi" />
-                <div>
-                  <h3>{item.nome}</h3>
-                  <p>{priceFormat(item.preco)}</p>
-                  <button
-                    type="button"
-                    onClick={() => dispatch(remove(item.id))}
-                  />
-                </div>
-              </CartItem>
-            </>
-          ))
-        )}
-        <Subtotal>
-          <p>Valor total</p>
-          <p>{priceFormat(sumCart())}</p>
-        </Subtotal>
-        <button>Continuar com a entrega</button>
-      </Sidebar>
-    </CartContainer>
+    <>
+      <CartContainer className={isOpen ? 'is-open' : ''}>
+        {!showCheckout && <Overlay onClick={closeCart} />}
+        <Sidebar>
+          {items.length < 1 ? (
+            <p>Não há ítens no seu carrinho.</p>
+          ) : (
+            items.map((item) => (
+              <>
+                <CartItem key={item.id}>
+                  <img src={item.foto} alt="oi" />
+                  <div>
+                    <h3>{item.nome}</h3>
+                    <p>{priceFormat(item.preco)}</p>
+                    <button
+                      type="button"
+                      onClick={() => dispatch(remove(item.id))}
+                    />
+                  </div>
+                </CartItem>
+              </>
+            ))
+          )}
+          <Subtotal>
+            <p>Valor total</p>
+            <p>{priceFormat(sumCart())}</p>
+          </Subtotal>
+          <button onClick={goCheckout}>Continuar com a entrega</button>
+        </Sidebar>
+      </CartContainer>
+
+      {showCheckout && <Checkout onClose={() => setShowCheckout(false)} />}
+    </>
   )
 }
 
---
+++ src/pages/Checkout/index.tsx
@@ -0,0 +1,23 @@
+const Checkout = ({ onClose }: { onClose: () => void }) => (
+  <div>
+    Pagamento - Valor a pagar R$ 0,00
+    <div>
+      <label htmlFor="cardName">Nome no cartão</label>
+      <input type="text" name="cardName" id="cardName" />
+
+      <label htmlFor="cardNumber">Número do cartão</label>
+      <input type="text" name="cardNumber" id="cardNumber" />
+
+      <label htmlFor="cvv">CVV</label>
+      <input type="text" name="cvv" id="cvv" />
+
+      <label htmlFor="expMonth">Mês de vencimento</label>
+      <input type="text" name="expMonth" id="expMonth" />
+
+      <label htmlFor="expYear">Ano de vencimento</label>
+      <input type="text" name="expYear" id="expYear" />
+    </div>
+  </div>
+)
+
+export default Checkout
